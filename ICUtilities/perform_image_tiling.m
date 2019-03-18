e4b81338897ecc8591105e8a942a089242575cc5
alexany <alexany@j8dq1l2c7.dhcp.thecrick.org>
2019-03-18 18:47:59 +0000

    ALYtools: bug fix in perform_image_tiling proc

--- ICUtilities/perform_image_tiling.m
+++ ICUtilities/perform_image_tiling.m
@@ -1,6 +1,6 @@
-function perform_image_tiling(in_src_dir,in_dst_dir,in_Extension,in_ncols,in_nrows,ovlp_x,ovlp_y,QT)
+function perform_image_tiling(in_src_dir,in_dst_dir,in_Extension,in_ncols,in_nrows,ovlp_x,ovlp_y,mode,QT)
 
-% perform_image_tiling('D:\Users\alexany\SMLM_2019\2019_02_04_Sunil_for_stitching\SLICE_2','D:\Users\alexany\SMLM_2019\2019_02_04_Sunil_for_stitching','ome.tif',5,1,0.5,0.5,0)
+resolve_xlwrite_path_issue();
 
 tic
 
@@ -9,6 +9,7 @@ dc.problem = 'Experimental';
 
 Extension = in_Extension; %'ome.tif';
 
+dc.ImageTiling_mode = mode;
 dc.ImageTiling_Ncols = in_ncols;
 dc.ImageTiling_Nrows = in_nrows;
 dc.ImageTiling_Ovlp_X = ovlp_x;
@@ -24,7 +25,7 @@ dc.load_multiple(fnames,in_src_dir,false);
 
 str = strsplit(in_src_dir,filesep);
 xlsname = [in_dst_dir filesep char(str(numel(str))) '.xls'];
-xlswrite( xlsname,[captions; datas]);
+xlwrite(xlsname,[captions; datas]);
 filesavename = [in_dst_dir filesep char(str(numel(str))) '.ome.tif'];
 bfsave(fig,filesavename);
 
@@ -32,3 +33,49 @@ disp(['execution time ' num2str(toc/60) ' min']);
 
 end
 
+
+%-------------------------------------------------------------%
+function resolve_xlwrite_path_issue()
+            jPath = javaclasspath('-dynamic');
+            % first check it isn't already in the dynamic path
+            WriteXLPath = false;
+            for i = 1:length(jPath)
+                if strfind(jPath{i},'jxl.jar');
+                    WriteXLPath = true;
+                    break;
+                end
+            end
+                
+            if ~WriteXLPath
+                path = which('jxl.jar');
+                if isempty(path)
+                    path = fullfile(fileparts(mfilename('fullpath')), 'jxl.jar');
+                end
+                if ~isempty(path) && exist(path, 'file') == 2
+                    javaaddpath(path);
+                else 
+                     assert('Cannot automatically locate an jxl JAR file');
+                end
+            end
+            
+             % first check it isn't already in the dynamic path
+            WriteXLPath = false;
+            for i = 1:length(jPath)
+                if strfind(jPath{i},'MXL.jar');
+                    WriteXLPath = true;
+                    break;
+                end
+            end
+                
+            if ~WriteXLPath
+                path = which('MXL.jar');
+                if isempty(path)
+                    path = fullfile(fileparts(mfilename('fullpath')), 'MXL.jar');
+                end
+                if ~isempty(path) && exist(path, 'file') == 2
+                    javaaddpath(path);
+                else 
+                     assert('Cannot automatically locate an MXL JAR file');
+                end
+            end           
+end
