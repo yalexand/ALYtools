a0dd79612afbc12edd451dc585eb419b3ba2dc6c
THECRICK\alexany <yalexand@ic.ac.uk>
2020-10-06 15:43:12 +0100

    ALYtools: SIFNE - filaments segmentation

--- Classes/@ALYtools_data_controller/do_SIFNE_Segmentation.m
+++ Classes/@ALYtools_data_controller/do_SIFNE_Segmentation.m
@@ -1,6 +1,5 @@
 function sgm = do_SIFNE_Segmentation(obj,send_to_Icy,~) 
                     
-
         tic        
             sgm = [];
             %
@@ -10,29 +9,32 @@ function sgm = do_SIFNE_Segmentation(obj,send_to_Icy,~)
             R                   = obj.SIFNE_LFT_OFT_Radius_of_Filter;
             NofOrientations_FT  = obj.SIFNE_LFT_OFT_Number_of_Filter_Orinetations;
                         
-            u = obj.imgdata;
+            u = single(obj.imgdata);
             s = u(u~=0);
             t = quantile(s(:),0.995);
             u(u>t)=t;
-            OriginImg = uint8(map(u,0,255));                                    
-            % OriginImg = imadjust(im2uint8(obj.imgdata)); % ehm that was wrong
+            OriginImg = uint8(map(u,0,255));
                                     
             [H, W] = size(OriginImg);
             OriginImg_Margin = uint8(zeros(H+R+R,W+R+R));
             OriginImg_Margin(R+1:H+R,R+1:W+R) = OriginImg;
             
-            %ROI_Mask = ones(size(OriginImg_Margin)); % !!!
             % try vulgar segmentation
             ROI_Mask = zeros(size(OriginImg_Margin));
             % segmnentation setups
             S = obj.SIFNE_vulgar_ROI_sgm_scale;
             t = obj.SIFNE_vulgar_ROI_sgm_threshold; 
-            % segmnentation setups - ends
+            % segmentation setups - ends
             u = single(obj.imgdata);
             u = gsderiv(u,S,0);
             u = map(u,0,1);
-                
             ROI_Mask(R+1:H+R,R+1:W+R) = u>t; 
+            %
+            % remove small patches
+            min_patch_size = round(obj.SIFNE_vulgar_ROI_sgm_min_patch_size/((obj.microns_per_pixel).^2)); %remove objects smaller than XX square microns
+            ROI_Mask = bwareaopen(ROI_Mask,min_patch_size);
+            % should be OK ...
+            ROI_Mask = fill_small_holes(ROI_Mask,round(min_patch_size/4));
             % vulgar segmentation - ends
 
             [OFT_Img, LFT_Img, LFT_Orientations] = LFT_OFT_mex(double(OriginImg_Margin),double(R),double(NofOrientations_FT),double(ROI_Mask));
@@ -40,40 +42,21 @@ function sgm = do_SIFNE_Segmentation(obj,send_to_Icy,~)
             sigma = obj.SIFNE_SGM_Junction_Size;
             OFT_Img = gsderiv(OFT_Img,sigma,0); % smoother !!
                                    
-% s = OFT_Img(OFT_Img>0);
-% t = quantile(s(:),0.05);
-% icy_imshow(OFT_Img/t,num2str(t));
-                        
-            OFT_Img = map(OFT_Img,0,1);
-            %                              
-            BW = OFT_Img>obj.SIFNE_SGM_Filaments_Threshold;
+            % global, dimensional threshold
+            BW = OFT_Img/(2*R)>obj.SIFNE_SGM_Filaments_Threshold;
             
             BW = imclose(BW,strel('disk',1)); % :) smoother!!
             
-            RawSke = bwmorph(BW,'thin',Inf);            
-%
-%try to improve patches segmentation basing on what is known about filaments
-            r = round(16*obj.SIFNE_SGM_Junction_Size);
-            ROI_Mask = imdilate(RawSke,strel('disk',r,0));
-          
-            % remove small patches...                        
-            % keep largest patch for the case
+            RawSke = bwmorph(BW,'thin',Inf);
+            
+            % remove empty patches
             L = bwlabel(ROI_Mask);
-            stats = regionprops(L,'Area');
-            areas = [stats.Area];
-            largest_patch = L==find(areas==max(areas));            
-            %
-            min_patch_size = round(obj.SIFNE_vulgar_ROI_sgm_min_patch_size/((obj.microns_per_pixel).^2)); %remove objects smaller than 110 square microns
-            ROI_Mask = bwareaopen(ROI_Mask,min_patch_size);
-            % should be OK ...
-            ROI_Mask = fill_small_holes(ROI_Mask,round(min_patch_size/4));
-            if 0==sum(ROI_Mask,'All')
-                ROI_Mask = largest_patch; % shouldn't happen too often
+            for k=1:max(L(:))
+                if 0==sum(RawSke.*(L==k),'All')
+                    ROI_Mask = ROI_Mask &~ (L==k); 
+                end
             end
-            RawSke = RawSke & ROI_Mask;
-            RawSke = bwmorph(RawSke,'clean');
-%try to improve patches segmentation basing on what is known about filaments
-%                                    
+                        
                     if send_to_Icy
                         [sx,sy]=size(ROI_Mask);
                         iv = zeros(H,W,3,1,1,'uint8');                        
@@ -96,5 +79,3 @@ function sgm = do_SIFNE_Segmentation(obj,send_to_Icy,~)
         
         disp('do_SIFNE_Segmentation');           
 end  
-
-
