b8b45590269c58f24615b1aa9cf50f8bd3e0c734
THECRICK\alexany <yalexand@ic.ac.uk>
2020-11-05 19:27:47 +0000

    fixes to previous commit

--- GUIDEInterfaces/t_dependent_Nuclei_ratio_FRET_HTS_MI_heatmapper.m
+++ GUIDEInterfaces/t_dependent_Nuclei_ratio_FRET_HTS_MI_heatmapper.m
@@ -22,10 +22,10 @@ function varargout = t_dependent_Nuclei_ratio_FRET_HTS_MI_heatmapper(varargin)
 
 % Edit the above text to modify the response to help t_dependent_Nuclei_ratio_FRET_HTS_MI_heatmapper
 
-% Last Modified by GUIDE v2.5 05-Nov-2020 14:17:19
+% Last Modified by GUIDE v2.5 05-Nov-2020 19:24:23
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 1;
+gui_Singleton = 0;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @t_dependent_Nuclei_ratio_FRET_HTS_MI_heatmapper_OpeningFcn, ...
@@ -1061,9 +1061,8 @@ end
 
 
 % --- Executes on button press in clustering_details.
-function clustering_details_Callback(hObject, eventdata, handles)
+function clustering_details_Callback(hObject, ~, handles)
     handles.IDX = do_clustering(handles,true);
     guidata(hObject, handles);
     visualize_platemap(hObject,handles);
     update_visuals_axis(hObject,handles);
-
