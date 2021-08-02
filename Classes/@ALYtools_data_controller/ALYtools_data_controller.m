55e86d4ea80e243f99c0eb49056688c9f46129ca
THECRICK\alexany <yalexand@ic.ac.uk>
2021-08-02 15:15:46 +0100

    ALYtools: bug fix for OPT_ZFish_Embryo

--- Classes/@ALYtools_data_controller/ALYtools_data_controller.m
+++ Classes/@ALYtools_data_controller/ALYtools_data_controller.m
@@ -321,7 +321,7 @@ classdef ALYtools_data_controller < handle
             OPT_ZFish_Embryo_sgm_K31               = 6; % scale ratio 3:1
             OPT_ZFish_Embryo_sgm_a1                = .0; %
             OPT_ZFish_Embryo_sgm_t                 = .9; %
-            OPT_ZFish_Embryo_sgm_min_vol           = 1757500; % um3            
+            OPT_ZFish_Embryo_sgm_min_vol           = 13824000; % um3            
             
             % SIFNE % SMLM Image Filament Network Extractor
             SIFNE_LFT_OFT_Radius_of_Filter = 10; % pixels
